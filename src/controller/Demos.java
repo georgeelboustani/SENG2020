142e846b20c961e4a969fdf3ca9263782c7257d1
George <George@192.168.2.11>
2012-10-15 02:54:50 +1100

    fdgdf

---
+++ src/controller/Demos.java
@@ -0,0 +1,60 @@
+package controller;
+
+public class Demos {}
+    
+/*
+  
+ AFTER INITIALISE
+     => Add category
+     => Add product
+     => Add supplier
+     => Order product
+     => Receive order // fix print
+    
+    
+1. Register a new product and enter n quantity of items to that product.
+    =>  Add product to system
+    =>  Order Product
+    =>  Receive Product
+
+
+
+2. Move x quantity of a particular product between the three store locations (warehouse, back
+store and shelves).
+    =>Transfer
+
+
+
+
+3. Show how a customer can “checkout” their trolley of products
+    
+    
+
+ 4. Different users 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    */
